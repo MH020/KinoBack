3b148d6150ee6d0c7690e1ffaff4334310cc80d2
MH020 <166133291+MH020@users.noreply.github.com>
2025-03-01 07:31:15 +0100

    Initial Commit

---
+++ KinoBack/src/main/java/com/example/kinoback/KinoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoBackApplication.class, args);
+    }
+
+}
