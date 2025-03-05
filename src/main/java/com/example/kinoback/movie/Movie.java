868645b0aea0e5ee26df8040d862d765cae25c96
jazzyzoney <jaqtash22@gmail.com>
2025-03-05 13:31:51 +0100

    Update Movie.java

--- src/main/java/com/example/kinoback/movie/Movie.java
+++ src/main/java/com/example/kinoback/movie/Movie.java
@@ -1,11 +1,82 @@
 package com.example.kinoback.movie;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import java.sql.Time;
 
 @Entity
 public class Movie {
 
+    @Id
+    @Column(length = 100)
+    private String title;
+    private int releaseYear;
+    private Time duration;
+    private int ageRestriction;
+    private int genre;
+    @Column(length = 100)
+    private String director;
+    @Column(length = 900)
+    private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "movieId", referencedColumnName = "id")
+    private Movie movie;
 
+    // getters and setters
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Time getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Time duration) {
+        this.duration = duration;
+    }
+
+    public int getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(int ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public int getGenre() {
+        return genre;
+    }
+
+    public void setGenre(int genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
