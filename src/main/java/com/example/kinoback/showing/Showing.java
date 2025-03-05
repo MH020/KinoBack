d3f5f6a83f938d71781da53a0049c1a42ec86f02
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-03-05 12:56:57 +0100

    #15 showing klasse variable oprettet. ingen relationer

--- src/main/java/com/example/kinoback/showing/Showing.java
+++ src/main/java/com/example/kinoback/showing/Showing.java
@@ -1,4 +1,20 @@
 package com.example.kinoback.showing;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
 public class Showing {
+    @Id
+    private int id;
+    private Time time;
+    private Date date;
+
+    
+    private int movieId;
+    private int theatreId;
+
 }
