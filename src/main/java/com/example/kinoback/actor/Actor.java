3d329643328faf23e7d7518de72fb3f95e25414f
MH020 <166133291+MH020@users.noreply.github.com>
2025-03-05 13:30:06 +0100

    Update Actor.java

--- src/main/java/com/example/kinoback/actor/Actor.java
+++ src/main/java/com/example/kinoback/actor/Actor.java
@@ -1,4 +1,41 @@
 package com.example.kinoback.actor;
+import jakarta.persistence.*;
 
+@Entity
 public class Actor {
+    @Id
+   private int id;
+
+    @Column(length = 100)
+  private String fullName;
+
+    @OneToMany
+    @JoinColumn(name = "ActorMovie", referencedColumnName = "");
+    private int actorId;
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(int actorId) {
+        this.actorId = actorId;
+    }
 }
+
