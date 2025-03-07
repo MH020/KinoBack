ef300997874804fb6aa933c6b1a5187f5298ae6c
MH020 <166133291+MH020@users.noreply.github.com>
2025-03-07 14:59:05 +0100

    #28 man kan nu se visninger på frontenden

--- src/main/java/com/example/kinoback/showing/ShowingController.java
+++ src/main/java/com/example/kinoback/showing/ShowingController.java
@@ -1,9 +1,14 @@
 package com.example.kinoback.showing;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:63343")
 @RequestMapping("/showing")
 public class ShowingController {
     private ShowingService showingService;
@@ -11,4 +16,11 @@ public class ShowingController {
     public ShowingController(ShowingService showingService){
         this.showingService = showingService;
     }
+
+
+    @GetMapping("/all")
+    public List<Showing> getShowings() {
+        return showingService.getShowings();
+    }
+
 }
