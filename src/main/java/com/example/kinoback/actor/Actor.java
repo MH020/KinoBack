ea8c145ecdf050043adc07d74dd84aa554e9a8e9
Jasmin <144851658+jazzyzoney@users.noreply.github.com>
2025-03-10 12:17:57 +0100

    Revert "Jpa database"

--- src/main/java/com/example/kinoback/actor/Actor.java
+++ src/main/java/com/example/kinoback/actor/Actor.java
@@ -1,39 +1,4 @@
 package com.example.kinoback.actor;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.*;
 
-import java.util.HashSet;
-import java.util.Set;
-
-@Entity
 public class Actor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private int id;
-
-    @Column(length = 100)
-  private String fullName;
-
-    @OneToMany(mappedBy = "actor")
-    @JsonBackReference
-    private Set<ActorMovie> actorMovies = new HashSet<>();
-
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
 }
-
--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
--- Inserting data into Movie table
-INSERT INTO Movie (title, duration, release_Year, age_Restriction, genre, director, description) VALUES
-('Movie A', 02:00:00, 2023, 13, 1, 'Director A', 'Description for Movie A'),
-('Movie B', 01:45:00, 2022, 18, 2, 'Director B', 'Description for Movie B');
-
