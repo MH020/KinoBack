21333b91c34c832579210c14d53185ccbde4d9e9
MH020 <166133291+MH020@users.noreply.github.com>
2025-03-10 12:58:02 +0100

    Revert "Merge branch 'main' into jpa-database"
    This reverts commit 672af9fa2b988692e1b53a079b9f53eab8f290bb, reversing
    changes made to 805d2ec3fd945e6ad86f7d01101955c284e892e5.

--- src/main/java/com/example/kinoback/movie/MovieController.java
+++ src/main/java/com/example/kinoback/movie/MovieController.java
@@ -1,7 +1,10 @@
 package com.example.kinoback.movie;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -10,6 +13,11 @@ public class MovieController {
 
     public MovieController(MovieService movieService){
         this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getMovies() {
+        return movieService.getMovies();
     }
 
 }
