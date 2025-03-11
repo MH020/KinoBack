fedcde580b9dc4f128a0463cf5daad34e633e9e8
MH020 <166133291+MH020@users.noreply.github.com>
2025-03-11 10:43:45 +0100

    #2 tilføjet cross origiin

--- src/main/java/com/example/kinoback/actor/ActorController.java
+++ src/main/java/com/example/kinoback/actor/ActorController.java
@@ -1,5 +1,6 @@
 package com.example.kinoback.actor;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:63343")
 @RequestMapping("/actor")
 public class ActorController {
 
